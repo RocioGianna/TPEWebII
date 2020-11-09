512a6d962c76cdc4371508dd0f8804c538ccbc18
RocioGianna <68743051+RocioGianna@users.noreply.github.com>
2020-11-09 17:30:54 -0300

    agregue comentarios

--- Controller/ControllerApi.php
+++ Controller/ControllerApi.php
@@ -49,11 +49,13 @@ require_once 'ControllerApiAbstract.php';
                 $this->view->response("Comentario id=$id not found", 404);
             }
         }
+    }
 /*
         public function getFormNote($params = null){ 
             $id_zapatilla = $params[':ID'];
             $item = $this->model->GetInfo($id_zapatilla);
             $this->vista->ShowFormComent($item); //esto va con un response
         }
+    
 */
 
--- routerApi.php
+++ routerApi.php
@@ -4,16 +4,15 @@ require_once 'Controller/ControllerApi.php';
 
 $r = new Router();
 
-$r->addRoute("comentarios", "GET", "ControllerApi", "getComents");
-
-$r->addRoute("coment/:ID", "GET", "ControllerApi", "getComent");
-$r->addRoute("comentario/:ID", "GET", "ControllerApi", "getComentProducto");
-$r->addRoute("deleteComentario/:ID", "DELETE", "ControllerApi", "deleteComent");
-$r->addRoute("comentar", "POST", "ControllerApi", "addComent");
+$r->addRoute("comentarios", "GET", "ControllerApi", "getComents"); //trae todos los comentarios
+$r->addRoute("coment/:ID", "GET", "ControllerApi", "getComent"); //trae un comentario especifico
+$r->addRoute("comentario/:ID", "GET", "ControllerApi", "getComentProducto"); //trae los comentarios de un producto
+$r->addRoute("deleteComentario/:ID", "DELETE", "ControllerApi", "deleteComent"); //borra un comentario
+$r->addRoute("comentar", "POST", "ControllerApi", "addComent"); //se puede comentar
 /*
 $r->addRoute("comentar/:ID", "GET", "ControllerApi", "getFormComent");
 $r->addRoute("notar/:ID", "GET", "ControllerApi", "getFormNote");*/
-*/
+
 
 
 $r->route($_GET['resource'], $_SERVER['REQUEST_METHOD']); 
