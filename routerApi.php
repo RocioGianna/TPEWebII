6e3d26f76a59ee416e7d4a3f5e7e2ebd14b741f0
RocioGianna <68743051+RocioGianna@users.noreply.github.com>
2020-11-12 13:44:52 -0300

    haciendo cambios al busqueda avanzada

--- routerApi.php
+++ routerApi.php
@@ -8,7 +8,8 @@ $r = new Router();
 
 /*$r->addRoute("comentarios", "GET", "ControllerApi", "getComents"); //trae todos los comentarios*/
 $r->addRoute("coment/:ID", "GET", "ControllerApi", "getComent"); //trae un comentario especifico
-$r->addRoute("opinion/:ID", "GET", "ControllerApi", "getFormOpinion"); //trae los comentarios de un producto
+$r->addRoute("opinion/:ID", "GET", "ControllerApi", "getFormOpinion"); 
+$r->addRoute("comentario/:ID", "GET", "ControllerApi", "getComentProducto");//trae los comentarios de un producto
 $r->addRoute("deleteComentario/:ID", "GET", "ControllerApi", "deleteComent"); //borra un comentario
 $r->addRoute("comentar", "POST", "ControllerApi", "addComent"); //se puede comentar
 
