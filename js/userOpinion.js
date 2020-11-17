325824394086a6fdd9566ad8464b7125f0faa043
Lionel Cassar <lionelcassar2501@gmail.com>
2020-11-17 10:22:41 -0300

    agregue a mi js la constante id que hiciste, hoy 17/11 veo lo de hacer una columna +info, que en funcion del usuario logeado o no, o del admin, se vea corectamente

--- js/userOpinion.js
+++ js/userOpinion.js
@@ -2,12 +2,14 @@
 
 document.addEventListener("DOMContentLoaded",  function (){
     function getComents(){
-    fetch('api/comentario/10')
+    const id = document.getElementById("id_producto").textContent;
+    fetch('api/comentario/' + id)
         .then(response => response.json())
         .then((comentarios) => renderComs(comentarios))
         .catch(error => console.log(error));
     }
     function renderComs(comentarios){
+        const id = document.getElementById("id_producto").textContent;
         const listComents = document.querySelector("#listComents");
         const listNotas = document.querySelector("#listNotas");
         for(let coment of comentarios){
@@ -17,11 +19,13 @@ document.addEventListener("DOMContentLoaded",  function (){
             for(let i = 0; i < btnDelete.length;i++){
                 btnDelete[i].addEventListener("click", 
                 function deleteComent(){
-                    fetch("api/deleteCom/62", {
+                    fetch("api/deleteCom/" + id, {
                         method: "delete",
                         headers: {"Content-type": "application/json"},
-                        body: getComents()
                     })
+                    .then(response => response.json())
+                    .then((comentarios) => renderComs(comentarios))
+                    .catch(error => console.log(error));
                     
                 });
             } 
@@ -32,13 +36,14 @@ document.addEventListener("DOMContentLoaded",  function (){
     let btnAddCom = document.querySelector("#btnAddCom");
     btnAddCom.addEventListener("click", 
     function addComent(){
+        const id = document.getElementById("id_producto").textContent;
         let comentario = document.querySelector("#comentario").value;
         let nota = document.querySelector("#notaItem").value;
         let opinion = {
             "comentario":comentario,
             "nota": nota
         };
-        fetch("api/comentar/10", {
+        fetch("api/comentar/" + id, {
             method: "post",
             headers: {"Content-type": "application/json"},
             body: JSON.stringify(opinion)
