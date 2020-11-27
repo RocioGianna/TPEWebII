5e7e98692a4f24a04d895f0af77f8de2850271e3
Lionel Cassar <lionelcassar2501@gmail.com>
2020-11-27 13:41:07 -0300

    se cambio el height de los li de las notas de los comentarios

--- templates_c/253d448bca451d8393ecbd46a88453cf98e143a9_0.file.formLogin.tpl.php
+++ templates_c/253d448bca451d8393ecbd46a88453cf98e143a9_0.file.formLogin.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-11-17 18:40:37
+/* Smarty version 3.1.34-dev-7, created on 2020-11-24 14:10:24
   from 'C:\xampp\htdocs\web2\flyshoes.com\TPEWebII\templates\formLogin.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5fb40b15efbbb3_42833078',
+  'unifunc' => 'content_5fbd0640dda996_46639095',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '253d448bca451d8393ecbd46a88453cf98e143a9' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web2\\flyshoes.com\\TPEWebII\\templates\\formLogin.tpl',
-      1 => 1605634835,
+      1 => 1606182534,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_5fb40b15efbbb3_42833078 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fbd0640dda996_46639095 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:userHeader.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 <div class="container">
